d49a0300733265390f65f5bd4c00bdfeec313f6f
Dastan Nurkanov <dastan989804@gmail.com>
2023-02-03 20:23:30 +0600

    qwree

--- flutter_test_application/lib/feature/presentation/screens/user_list_screen/widgets/zxc.dart
+++ flutter_test_application/lib/feature/presentation/screens/user_list_screen/widgets/zxc.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -7,6 +8,6 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(child: Text('asd'),);
   }
 }
