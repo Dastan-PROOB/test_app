169ebca2f3f95ac9de00ee10f06eeaa965a66392
Dastan Nurkanov <dastan989804@gmail.com>
2023-02-03 20:33:05 +0600

    asddfs

--- flutter_test_application/lib/feature/presentation/screens/user_list_screen/widgets/zxc.dart
+++ flutter_test_application/lib/feature/presentation/screens/user_list_screen/widgets/zxc.dart
@@ -8,6 +8,6 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('asd'),);
+    return Container(child: Text('asasdd'),);
   }
 }
